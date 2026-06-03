9790ca36a13eb7afa9359d0e44edc126fa7c7335
Pratyay Banerjee <pratyaybanerjee2003@gmail.com>
2026-06-03 13:29:35 +0530

    Add files via upload

---
+++ frontend/src/components/layout/Layout.tsx
@@ -0,0 +1,25 @@
+import { useEffect } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
+import Navbar from './Navbar';
+import Footer from './Footer';
+import AnimatedBackground from './AnimatedBackground';
+
+export default function Layout() {
+  const { pathname, hash } = useLocation();
+
+  useEffect(() => {
+    if (!hash) {
+      window.scrollTo(0, 0);
+    }
+  }, [pathname, hash]);
+  return (
+    <div className="min-h-screen flex flex-col font-sans text-navy-900 relative">
+      <AnimatedBackground />
+      <Navbar />
+      <main className="flex-grow pt-20">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+}
